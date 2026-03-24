c12d58c238975546d5eb303a79bd7756ec7ad4fd
surojpaul <paulsuroj87@gmail.com>
2026-03-24 16:16:21 +0530

    Remove old loop file

--- keep_going_loop.py
+++
@@ -1,8 +0,0 @@
-keep_going = "YES"
-count = 0
-while keep_going == "YES":
-    count = count + 1
-    print("Keep quiet and practice")
-    keep_going = input("Do you want me to continue, YES/NO ").strip().upper()
-print(f"Total session completed {count}")
-print("Stop and Rest")
