dbda88d898d242921257455ad6a67d52287bf056
surojpaul <paulsuroj87@gmail.com>
2026-03-28 20:55:08 +0530

    Clean up: removed old python script files.

--- test_matplotlib.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-days = [1, 2, 3, 4, 5]
-temperature = [30, 32, 28, 31, 29]
-plt.plot(days, temperature)
-plt.xlabel('Days')
-plt.ylabel('Temperature (°C)')
-plt.title('Temperature over Days')
-plt.show()
